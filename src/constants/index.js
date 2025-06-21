ac70a76d06bb50b6604cd3c1825f180c60793513
Tuans09 <dt2003911@gmail.com>
2025-06-21 18:27:53 +0700

    sua

--- src/constants/index.js
+++ src/constants/index.js
@@ -15,14 +15,14 @@ const navLinks = [
 ];
 
 const words = [
-  { text: "Ideas", imgPath: "/images/ideas.svg" },
-  { text: "Concepts", imgPath: "/images/concepts.svg" },
-  { text: "Designs", imgPath: "/images/designs.svg" },
   { text: "Code", imgPath: "/images/code.svg" },
   { text: "Ideas", imgPath: "/images/ideas.svg" },
-  { text: "Concepts", imgPath: "/images/concepts.svg" },
-  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Build", imgPath: "/images/concepts.svg" },
+  { text: "Plan", imgPath: "/images/designs.svg" },
   { text: "Code", imgPath: "/images/code.svg" },
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Build", imgPath: "/images/concepts.svg" },
+  { text: "Plan", imgPath: "/images/designs.svg" },
 ];
 
 const counterItems = [
